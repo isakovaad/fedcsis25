0ad29183fea7375d821a3989eab77f1e80034acf
bekzod-amonov <bekzod.amonov.study@gmail.com>
2025-05-23 22:40:42 +0200

    list of pieces in a board

---
+++ fedcsis25/feature_engineering.py
@@ -0,0 +1,8 @@
+
+
+
+# list of pieces in a board
+pieces_black = ['q', 'r', 'b', 'n', 'p']
+pieces_white = ['Q', 'R', 'B', 'N', 'P']
+pieces_value = [9, 7, 3, 3, 1]
+pieces_complexity = [12, 7, 5, 4, 1]
